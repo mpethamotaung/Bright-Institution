c353ad4ccd32bfa3c2bd71a4caad046a4d75fbde
Hanma-yujiro <mpethamotaung@gmail.com>
2024-07-22 14:58:07 +0200

    create CustomUser model that extends Django's AbstractUser(to add custom fields and roles).

--- tutoring_platform/authentication/models.py
+++ tutoring_platform/authentication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    USER_TYPES = (
+        ('student', 'Student'),
+        ('tutor', 'Tutor'),
+        ('admin', 'Admin'),
+    )
+    user_type = models.CharField(max_length=10, choices=USER_TYPES, default='student')
