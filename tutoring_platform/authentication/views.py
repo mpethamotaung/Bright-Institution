f1c32e8f34b716bc3c572678eb6521711e8c4123
Hanma-yujiro <mpethamotaung@gmail.com>
2024-07-22 11:56:21 +0200

    create user registration function view in authentication app

--- tutoring_platform/authentication/views.py
+++ tutoring_platform/authentication/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = UserCreationForm()
+    return render(request, 'authentication/register.html', {'form': form})
