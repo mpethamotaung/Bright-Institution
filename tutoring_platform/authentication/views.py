9f6c8bf03dc327d88a32ea50652da562db332038
Hanma-yujiro <mpethamotaung@gmail.com>
2024-07-22 13:49:01 +0200

    create login function view in authentication app views

--- tutoring_platform/authentication/views.py
+++ tutoring_platform/authentication/views.py
@@ -13,3 +13,14 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'authentication/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'authentication/login.html', {'form': form})
