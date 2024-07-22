cabedb85c60d018b72fd19fec22bd04eb58477ee
Hanma-yujiro <mpethamotaung@gmail.com>
2024-07-22 14:31:03 +0200

    create logout url path in authentication app urls

--- tutoring_platform/authentication/urls.py
+++ tutoring_platform/authentication/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
