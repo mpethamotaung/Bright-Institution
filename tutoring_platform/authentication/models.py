294aec90e6b9686c35ac44084930136c8b5c0c04
Hanma-yujiro <mpethamotaung@gmail.com>
2024-07-23 09:46:34 +0200

    recreate user models for authentication app

--- tutoring_platform/authentication/models.py
+++ tutoring_platform/authentication/models.py
@@ -2,10 +2,3 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class CustomUser(AbstractUser):
-    USER_TYPES = (
-        ('student', 'Student'),
-        ('tutor', 'Tutor'),
-        ('admin', 'Admin'),
-    )
-    user_type = models.CharField(max_length=10, choices=USER_TYPES, default='student')
