94c43bb094330253a1b8f97b5aed69fb658fdd2e
Hanma-yujiro <mpethamotaung@gmail.com>
2024-08-07 10:06:39 +0200

    define models for booking and subjects

--- tutoring_platform(project)/tutoring_platform/bookings/models.py
+++ tutoring_platform(project)/tutoring_platform/bookings/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Booking(models.Model):
+    student = models.ForeignKey(CustomUser, related_name='student_bookings', on_delete=models.CASCADE)
+    tutor = models.ForeignKey(CustomUser, related_name='tutor_bookings', on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    confirmed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.student.username} - {self.tutor.username} on {self.date}"
