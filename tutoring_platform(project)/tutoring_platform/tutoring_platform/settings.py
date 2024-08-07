91c42716546aa04f138226c824c66cc8ea320e30
Hanma-yujiro <mpethamotaung@gmail.com>
2024-08-07 09:54:25 +0200

    add packages and created apps to main tutoring_platfrom app

--- tutoring_platform(project)/tutoring_platform/tutoring_platform/settings.py
+++ tutoring_platform(project)/tutoring_platform/tutoring_platform/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'users',
+    'bookings',
+    'payments',
 ]
 
 MIDDLEWARE = [
